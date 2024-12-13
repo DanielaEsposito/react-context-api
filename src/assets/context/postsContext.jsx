bc559c7f4a27474e1618ec2f5d2c2c42879a6d26
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-12-13 15:07:08 +0100

    add file context

---
+++ src/assets/context/postsContext.jsx
@@ -0,0 +1,64 @@
+import { createContext, useContext } from "react";
+//creo il context
+const PostContext = createContext();
+// esporto il provider
+export const postContextProvider = () => {
+  const postsData = {
+    posts: [
+      {
+        id: 1,
+        title: "Ciambellone",
+        category: "Dolce",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolorem beatae voluptates nam! Repudiandae molestia beatae, quasi eos natus tempora, autem reiciendis doloribus porro vitae recusandae quam qui perferendis ratione!",
+        img: "/img/ciambellone.jpeg",
+        tags: ["ciambellone", "dolce"],
+        published: false,
+      },
+      {
+        id: 2,
+        title: "Cracker alla barbabietola",
+        category: "Snack",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolorem beatae voluptates nam! Repudiandae molestia beatae, quasi eos natus tempora, autem reiciendis doloribus porro vitae recusandae quam qui perferendis ratione!",
+        img: "/img/cracker_barbabietola.jpeg",
+        tags: ["snack", "salato"],
+        published: false,
+      },
+      {
+        id: 3,
+        title: "Pane fritto",
+        category: "Salato",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolorem beatae voluptates nam! Repudiandae molestia beatae, quasi eos natus tempora, autem reiciendis doloribus porro vitae recusandae quam qui perferendis ratione!",
+        img: "/img/cracker_barbabietola.jpeg",
+        tags: ["pane", "cina"],
+        published: false,
+      },
+      {
+        id: 4,
+        title: "Pasta alla barbabietola",
+        category: "Pasta",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolorem beatae voluptates nam! Repudiandae molestia beatae, quasi eos natus tempora, autem reiciendis doloribus porro vitae recusandae quam qui perferendis ratione!",
+        img: "/img/pasta_barbabietola.jpeg",
+        tags: ["pasta", "barbabietola", "rosso"],
+        published: false,
+      },
+      {
+        id: 5,
+        title: "Torta paesana",
+        category: "Dolce",
+        content:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolorem beatae voluptates nam! Repudiandae molestia beatae, quasi eos natus tempora, autem reiciendis doloribus porro vitae recusandae quam qui perferendis ratione!",
+        img: "/img/torta_paesana.jpeg",
+        tags: ["tradizione", "torta", "dolce"],
+        published: false,
+      },
+    ],
+  };
+
+  return <PostContext.provider value={postsData}></PostContext.provider>;
+};
+//esporto lo use
+export const usePostsContext = () => useContext(PostContext);
